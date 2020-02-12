a5b3817073572a322a1d5cdb148edb20ff807a1e
Fatma Dogan and Jieun Kang <jieun@>
2020-02-12 14:41:40 -0800

    seperate UI and Back-end logic

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { BikeService } from './bike-service';
 // import './bike-service';
 $(document).ready(function() {
   $("form").submit(function() {
@@ -9,30 +10,32 @@ $(document).ready(function() {
     let city = $("#location").val();
     $("#location").val("");
    
+    // (async () => {
+    //   let response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity&access_token=api_key`);
+    //   console.log(response);
+    //   let jsonifiedResponse = await response.json();
+    //   getElements(jsonifiedResponse);
+    //   console.log(jsonifiedResponse);
+    // }) ();
+
     (async () => {
-      let response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity&access_token=api_key`);
-      console.log(response);
-      let jsonifiedResponse = await response.json();
-      getElements(jsonifiedResponse);
-      console.log(jsonifiedResponse);
-    }) ();
+      let bikeServise = new BikeService();
+      const response = await bikeServise.searchBikeByCity(city);
+      getElements(response);
+    })();
 
     function getElements(response) {
       const bikesArr = response.bikes;
       
       for (var i = 0; i<bikesArr.length; i++) {
-        $('#bikeInfo').append(`<tr id ="bike-table" >
+        $('#bikes').append(`<tr id ="bike-table" >
         <td><img class="img-size" src='${response.bikes[i].large_img}'></td>
         <td> ${response.bikes[i].manufacturer_name} </td>
         <td>${response.bikes[i].stolen_location}</td></tr><br>
         `);
-        // $('#bikeLocation').append(``);
-        // $('#bikeImg').append(`<li><img class="img-size" src='${response.bikes[i].large_img}'></li>`);
       } 
     }
-    
-
- });
+  });
 });                                              
   // $(document).ready(function() {
   //   $("form").submit(function(event) {
