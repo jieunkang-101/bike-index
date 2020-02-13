e6c573001759f071804eb3e20bc2cda0a4c7c265
Student <example@example.com>
2020-02-13 14:13:17 -0800

    add for loof for counting most popular manufacturer

--- src/bike-service.js
+++ src/bike-service.js
@@ -1,17 +1,26 @@
+
+
 export class BikeService {
   async searchBikeByCity(city) {
     try {
-      let response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=proximity&access_token=api_key`);
-      let jsonifiedResponse = await response.json();
-      console.log(jsonifiedResponse);
-      if ( response.status !== 200 ) { 
-        return new Error("Not a valid information!")
-      }
+      let response = await fetch(`https://bikeindex.org:443/api/v3/search?&location=${city}&distance=10&stolenness=proximity&access_token=api_key`);
+      let jsonifiedResponse;
+      if ( response.ok && response.status === 200 ) { 
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      } 
       return jsonifiedResponse;
     } catch(error) {
-      console.error("There was an error handling your request: " + error.message)
-    }
+      return false;
+    } 
   }
+
+
+
+
+
+
 
   async countBike() {
   let response  = await fetch(`https://bikeindex.org:443/api/v3/search/count?location=IP&distance=10&stolenness=stolen&access_token=api_key`);
--- src/main.js
+++ src/main.js
@@ -15,24 +15,48 @@ $(document).ready(function() {
       const response = await bikeServise.searchBikeByCity(city);
       getElements(response);
     })();
-
+    
     function getElements(response) {
       const bikesArr = response.bikes;
+      const manufacturerArr = [];
+      console.log(manufacturerArr);
+      if (response) {
+        for (let i = 0; i<bikesArr.length; i++) {
+          manufacturerArr.push(`${response.bikes[i].manufacturer_name}`);
+
+          $('#bikes').append(`<tr id ="bike-table" >
+          <td><img class="img-size" src='${response.bikes[i].large_img}'></td>
+          <td> ${response.bikes[i].manufacturer_name} </td>
+          <td>${response.bikes[i].stolen_location}</td>
+          <td>${response.bikes[i].frame_colors}</td></tr><br>
+          `);
+        }
+      } else {
+        $('#bikes').text("There was an error handling your request.");
+      }
       
-      for (var i = 0; i<bikesArr.length; i++) {
-        $('#bikes').append(`<tr id ="bike-table" >
-        <td><img class="img-size" src='${response.bikes[i].large_img}'></td>
-        <td> ${response.bikes[i].manufacturer_name} </td>
-        <td>${response.bikes[i].stolen_location}</td></tr><br>
-        `);
-      } 
+      let mostManufac = 1;
+      let count = 0;
+      let manufacturerName;
+      for (let i=0; i<manufacturerArr.length; i++) {
+        for (let j=0; j<manufacturerArr.length; j++) {
+          if (manufacturerArr[i] === manufacturerArr[j]) {
+            count++;
+          }
+          if ( mostManufac < count ) {
+            mostManufac = count;
+            manufacturerName = manufacturerArr[i];
+          }
+        }
+        count = 0;
+        console.log(manufacturerName);
+      } return manufacturerName;
     }
   });
 
   $("#learn-btn").click(function(){
     event.preventDefault();
     
-
     (async () => {
       let bikeServise = new BikeService();
       const response = await bikeServise.countBike();
